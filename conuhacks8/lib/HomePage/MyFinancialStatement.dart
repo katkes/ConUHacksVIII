45896168e2c9776c8d5c3447b84e5d37154cf407
Christopher_Mezzacappa <christophermezzacappa818@yahoo.ca>
2024-01-21 04:09:40 -0500

    ExpenseReport

--- conuhacks8/lib/HomePage/MyFinancialStatement.dart
+++ conuhacks8/lib/HomePage/MyFinancialStatement.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'BottomNav.dart';
 
@@ -6,7 +8,7 @@ class MyFinancialStatement extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My Financial Statement'),
+        title: const Text('Financial Statement'),
         backgroundColor: Colors.blue.shade900,
       ),
       body: Container(
@@ -17,8 +19,59 @@ class MyFinancialStatement extends StatelessWidget {
             end: Alignment.bottomRight,
           ),
         ),
-        child: Center(
-          child: Text('My Financial Statement'),
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              // Financial Statement Title
+              Text(
+                'Yearly Financial Statement\nIncome: 1,000,000',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 16),
+
+              // Row with Two Columns
+              Row(
+                children: [
+                  // First Column
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Income: 100,000',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  // Second Column
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Expenses',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text('Cost of Goods Sold: \$50,000'),
+                        Text('Operating Expenses: \$20,000'),
+                        // Add more expense items as needed
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomNavBar(),
